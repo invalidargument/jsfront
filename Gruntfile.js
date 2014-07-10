5c45a36b31e7cfe160fc2816115ba7fb20b49dca
invalidargument <verstaerker@gmx.net>
2014-07-10 22:02:18 +0200

    change Gruntfile for clean up and copy files to build dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,19 +4,36 @@ module.exports = function(grunt) {
   });
   
   grunt.registerTask('default', 'Deploy project.', function() {
-      grunt.log.write('Copy minified external libraries. Build for Deployment. ...');
-      grunt.task.run('prepare');
+      grunt.log.write('Build, deploy...');
+      grunt.task.run('clean');
       grunt.task.run('build');
   });
   
-  grunt.registerTask('prepare', 'Copy minified external libraries.', function() {
-      grunt.file.copy('js/jquery/dist/jquery.min.js', 'js/rudibieller/lib/jquery.js');
-      grunt.file.copy('js/knockout/dist/knockout.js', 'js/rudibieller/lib/knockout.js');
-      grunt.file.copy('js/lodash/dist/lodash.min.js', 'js/rudibieller/lib/lodash.js');
-      grunt.file.copy('js/requirejs/require.js', 'js/rudibieller/lib/require.js');
+  grunt.registerTask('clean', 'Clean build directory.', function() {
+      grunt.file.expand({}, ['build/*']).forEach(function(path) {
+          grunt.log.write('Deleting ' + path).ok();
+          grunt.file.delete(path);
+      });
   });
   
   grunt.registerTask('build', 'Build for Deployment.', function() {
-      grunt.log.write('TODO' + "\n").ok();
+      var sourceDir = 'js/rudibieller/';
+      
+      grunt.file.copy('js/jquery/dist/jquery.min.js', sourceDir + 'lib/jquery.js');
+      grunt.file.copy('js/knockout/dist/knockout.js', sourceDir + 'lib/knockout.js');
+      grunt.file.copy('js/lodash/dist/lodash.min.js', sourceDir + 'lib/lodash.js');
+      grunt.file.copy('js/requirejs/require.js', sourceDir + 'lib/require.js');
+      
+      grunt.file.mkdir('build/js/lib');
+      grunt.log.write('Copying index.html').ok();
+      grunt.file.copy('index.html', 'build/index.html');
+      grunt.log.write('Copying js/rudibieller/main-built.js').ok();
+      grunt.file.copy(sourceDir + 'main-built.js', 'build/js/rudibieller.js');
+      
+      grunt.file.expand({}, ['js/rudibieller/lib/*']).forEach(function(path) {
+          grunt.log.write('Copying ' + path).ok();
+          var filename = path.split(sourceDir + 'lib/')[1]; /* BOOO! */
+          grunt.file.copy(path, 'build/js/lib/' + filename);
+      });
   });
 };
