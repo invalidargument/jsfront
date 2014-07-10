788479a2c4482dc803f4972cc421fda9f17e39b1
invalidargument <verstaerker@gmx.net>
2014-07-10 22:11:13 +0200

    remove useless echos

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "########### Start build ###########"
-echo "Uglify and pack local javascript..."
 node deployment/r.js -o js/rudibieller/build.js
 grunt
-echo "Build project, copy all prepared files to build directory..."
-echo "Build project, set paths and variables..."
-echo "########### Build done ###########"
